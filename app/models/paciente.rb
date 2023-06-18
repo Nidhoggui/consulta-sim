1edf5858a89efc1137757717a46d1b4a3233ffa2
Nidhoggui <guilhermemelquiades42@gmail.com>
2023-06-18 00:00:29 -0300

    fixing pacient bug and super styling the page

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -5,13 +5,12 @@ class Paciente < ApplicationRecord
 
   validates :nome, presence: true
   validates :cpf, presence: true, uniqueness: true, format: { with: /\A\d+\z/, message: "deve conter apenas números" }
-  validates :data_nascimento, presence: true, date: { before_or_equal_to: -> { Date.current }, message: "não aceitamos viajantes do tempo" }
   validate :data_nascimento_valida
 
   private
 
   def data_nascimento_valida
-    if data_nascimento.present? && data_nascimento.year > Date.current.year
+    if data_nascimento.present? && data_nascimento > Date.current
       errors.add(:data_nascimento, "não aceitamos viajantes do tempo")
     end
   end
