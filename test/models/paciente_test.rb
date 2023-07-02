086800bd83f1aeb3cfe84a2c25be34d2036ebdfe
Nidhoggui <guilhermemelquiades42@gmail.com>
2023-07-02 10:31:44 -0300

    add test for Paciente validations

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @paciente = pacientes(:one)
+  end
+
+  test "should not be valid with a future data_nascimento" do
+    @paciente.data_nascimento = Date.tomorrow
+    assert_not @paciente.valid?
+    assert_includes @paciente.errors[:data_nascimento], "não aceitamos viajantes do tempo"
+  end
+
+  test "should not be valid with a duplicate cpf" do
+    other_paciente = pacientes(:two)
+    @paciente.cpf = other_paciente.cpf
+    assert_not @paciente.valid?
+    assert_includes @paciente.errors[:cpf], "já está em uso"
+  end
 end
