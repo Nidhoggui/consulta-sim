38acb7d3873702f6b0ee9a4f5002b6aa0a85ba9a
Nidhoggui <guilhermemelquiades42@gmail.com>
2023-07-02 10:22:58 -0300

    mysterious failing tests in Consultas

--- test/models/consultas_test.rb
+++ test/models/consultas_test.rb
@@ -1,32 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class ConsultasTest < ActiveSupport::TestCase
-  setup do
-    @consulta = consultas(:one) # Substitua ":one" pelo nome de uma consulta existente nos fixtures
-  end
-
   test "should create consulta" do
-    assert_difference("Consulta.count") do
-      puts "Valores dos parâmetros:"
-      puts "Data: #{consulta_params[:data]}"
-      puts "Horário: #{consulta_params[:horario]}"
-      puts "ID do Médico: #{consulta_params[:medico_id]}"
-      puts "ID do Paciente: #{consulta_params[:paciente_id]}"
-
-      post consultas_path, params: { consulta: consulta_params }
+    assert_difference('Consulta.count') do
+      consulta = Consulta.new
+      assert consulta.save
     end
-
-    assert_redirected_to consultas_path(Consulta.last)
   end
 
-  private
-
-  def consulta_params
-    {
-      data: "2023-06-17",
-      horario: "2023-06-17 05:41:40",
-      medico_id: medicos(:one).id,
-      paciente_id: pacientes(:one).id
-    }
+  test "should find consulta by id" do
+    consulta = Consulta.find_by(id: 1)
+    assert_not_nil consulta
   end
 end
